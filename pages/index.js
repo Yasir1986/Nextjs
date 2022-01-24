a8e3b2658ad1fd9234c38f082cb11a0a228614e0
Yasir1986 <yasir.akbar86@hotmail.com>
2022-01-24 22:36:36 +0000

    layout added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,12 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Web Dev Newz</title>
+        <meta name="keywords" 
+        content="web develpement programming"
+        />
+      </Head>
      <h1>Welcome to Next</h1>
     </div>
   )

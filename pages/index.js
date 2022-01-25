8ec15c63204389bd29af2fd509d6b0ae6bc77aff
Yasir1986 <yasir.akbar86@hotmail.com>
2022-01-25 15:16:04 +0000

    add styles

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Web Dev Newz</title>
-        <meta name="keywords" 
-        content="web develpement programming"
-        />
+        <meta name="keywords" content="web develpement programming" />
       </Head>
-     <h1>Welcome to Next</h1>
+      <h1>Welcome to Next</h1>
     </div>
-  )
+  );
 }

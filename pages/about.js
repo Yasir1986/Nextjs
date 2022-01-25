1fc744d5d1799f6b166fb28d9e5e9062d792391b
Yasir1986 <yasir.akbar86@hotmail.com>
2022-01-25 14:40:11 +0000

    nav bar added

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 const about = () => {
-    return (
-        <div>
-            <Head>
-                <title>About</title>
-            </Head>
-            <h1>About Page</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>About Page</h1>
+    </div>
+  );
+};
 
 export default about;

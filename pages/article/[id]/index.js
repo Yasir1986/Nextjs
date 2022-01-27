330d1e5127fb615e1e9523624625f849d0498729
Yasir1986 <yasir.akbar86@hotmail.com>
2022-01-27 12:58:52 +0000

    getServerSideProps added

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,11 +1,26 @@
 import { useRouter } from "next/router";
 
-const article = () => {
-  // article id is added 
-  const router = useRouter();
-  const { id } = router.query;
+const article = ({ article }) => {
+  // article id is can be added via React way
+  // const router = useRouter();
+  // const { id } = router.query;
 
-  return <div>This is an article {id}</div>;
+  return <div>This is article {article.id}</div>;
+};
+
+// id can be added via NextJs getServerSideProps
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
 };
 
 export default article;
